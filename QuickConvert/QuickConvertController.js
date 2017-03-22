53e718da0a97f6827431c5f5363b6f6b584f974f
nithesh1992 <nithesh.chakravarthi@gmail.com>
2017-03-21 19:25:37 -0700

    initial commit
    QuickConvert and Lightning Header Added.

---
+++ QuickConvert/QuickConvertController.js
@@ -0,0 +1,32 @@
+({
+    
+    init : function(component, event, helper) {
+        
+    },
+    
+    ConvertIt : function(component, event, helper) {
+    	var action = component.get("c.convertIt");
+      	action.setParams({"recordId": component.get("v.recordId")});
+        action.setCallback(this, function(res) {
+        var response = res.getReturnValue();
+        var state = action.getState();
+        console.log(response);
+        if(component.isValid() && state == "SUCCESS"){            
+            var toastEvent = $A.get("e.force:showToast");
+            toastEvent.setParams({
+                  "title": "Success!",
+                "message": "This Lead has been converted.",
+                  "type ": "success"
+            });
+
+            toastEvent.fire(); 
+            $A.get("e.force:refreshView").fire();
+        
+            
+         } else if (state == "ERROR") {
+            console.log('There was a problem and the state is: '+ action.getState());
+         }
+      });
+      $A.enqueueAction(action);
+    }
+})

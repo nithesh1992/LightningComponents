32fd1369e03ba0a9c3b474f1c066224318350016
nithesh1992 <nithesh.chakravarthi@gmail.com>
2017-04-30 14:52:40 -0700

    Small modification
    Made as AppHostable only (doesn’t work as lightning action anymore)
    Added Toast Actions for success and error cases.
    You will be navigated to Account record after conversion.

--- QuickConvert/QuickConvertController.js
+++ QuickConvert/QuickConvertController.js
@@ -1,17 +1,18 @@
 ({
     
     init : function(component, event, helper) {
-        
+          
     },
     
     ConvertIt : function(component, event, helper) {
-    	var action = component.get("c.convertIt");
-      	action.setParams({"recordId": component.get("v.recordId")});
+      var action = component.get("c.convertIt");
+        action.setParams({"recordId": component.get("v.recordId")});
         action.setCallback(this, function(res) {
         var response = res.getReturnValue();
         var state = action.getState();
         console.log(response);
-        if(component.isValid() && state == "SUCCESS"){            
+        if(component.isValid() && state == "SUCCESS"){          
+            
             var toastEvent = $A.get("e.force:showToast");
             toastEvent.setParams({
                   "title": "Success!",
@@ -20,11 +21,20 @@
             });
 
             toastEvent.fire(); 
-            $A.get("e.force:refreshView").fire();
-        
+            var navEvt = $A.get("e.force:navigateToSObject");
+            navEvt.setParams({
+                "recordId": response
+            });
+            navEvt.fire();
             
          } else if (state == "ERROR") {
             console.log('There was a problem and the state is: '+ action.getState());
+            var toastEvent = $A.get("e.force:showToast");
+            toastEvent.setParams({
+                  "title": "Oops!",
+                "message": "Something went Wrong!",
+                  "type ": "error"
+            });
          }
       });
       $A.enqueueAction(action);
